d58d178eefdc7c081d7164ac9e2bcbf8924db50a
MariaLanss <maruska0990@gmail.com>
2023-09-03 20:38:27 +0300

    Домашка 4.3 /2

--- src/main/resources/liquibase/scripts/course-four-lesson-three.sql
+++ src/main/resources/liquibase/scripts/course-four-lesson-three.sql
@@ -1,4 +1,7 @@
+-- liquibase formatted sql
 
+-- changeset avramenko:1
 CREATE INDEX student_name_index ON student(name);
 
+-- changeset avramenko:2
 CREATE INDEX faculty_name_color_index on faculty(name, color);
